3e42912eb62038af799ca6356e4a7df60e5fb328
tanawin-sg <30564500+tanawin-sg@users.noreply.github.com>
2021-01-21 23:58:17 +0700

    Header Footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //Function compnent
 const Footer = () => {
-    return <h1>Footer</h1>
+    return <h1>Footer2</h1>
 }
 
 export default Footer;
--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //Function compnent
 const Header = () => {
-    return <h1>HEADER</h1>
+    return <h1>HEADER2</h1>
 }
 
 export default Header;
